726693f2900e8fb6d1fcac98be64aba2f4435370
allen <allenbw7@gamil.com>
2018-03-01 13:52:07 +0800

    3.1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let list = [
     {
-        title: '任务项一'
+        title: '任务项一',
+        isChecked: false //状态false为未选中状态
     },
     {
-        title: '任务项二'
+        title: '任务项二',
+        isChecked: true //状态true为选中状态
     }
 ];
 
@@ -11,14 +13,55 @@ let vm = new Vue({
     el: '.main',
     data: {
         list: list,
-        todo: ''
+        todo: '',
+        editorTodos: '',
+        oldTitle: ''
+    },
+    computed: {
+        noCheckedLength() {
+            return this.list.filter(function(item) {
+                return !item.isChecked;
+            }).length;
+        }
     },
     methods: {
-        addTodo() {
-            this.list.push({
-                title: this.todo
-            })
-            this.todo = '';
+        //添加任务
+        addTodo(todo) {
+            if(todo) {
+                this.list.push({
+                    title: this.todo,
+                    isChecked: false
+                })
+                this.todo = '';
+            }
+        },
+        //删除任务
+        deleteTodo(todo) {
+            let index = this.list.indexOf(todo);
+            this.list.splice(index,1);
+        },
+        //编辑任务
+        editorTodo(todo) {
+            this.oldTitle = todo.title;
+            this.editorTodos = todo;
+        },
+        //编辑成功
+        editorTodoed() {
+            this.editorTodos = '';
+        },
+        //取消编辑
+        cancelTodo(todo) {
+            todo.title = this.oldTitle;
+            this.editorTodos = '';
+        }
+    },
+    directives: {
+        'focus': {
+            update(el,binding) {
+                if(binding.value) {
+                    el.focus();
+                }
+            }
         }
     }
 })
